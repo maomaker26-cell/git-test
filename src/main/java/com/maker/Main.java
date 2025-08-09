d2a79e9efff2340833544a5041a4102035ac1b5e
maker <maomaker@gmail.com>
2025-08-09 17:01:30 +0800

    three commit

--- src/main/java/com/maker/Main.java
+++ src/main/java/com/maker/Main.java
@@ -2,7 +2,7 @@ package com.maker;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("你好");
+        System.out.println("你好12");
         System.out.println("Hello world!");
         System.out.println("你好git");
     }
